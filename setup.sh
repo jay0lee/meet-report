8acebb5c91497837679e141aeec02aa5b3e08d44
Jay Lee <jay0lee@gmail.com>
2020-04-23 11:46:59 -0400

    few fixes to setup

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 # Clone meet-report
 git clone https://github.com/jay0lee/meet-report.git
 
-# Install GAM, we'll use it to setup project and authorize
-bash <(curl -s -S -L https://git.io/install-gam)
+# Install GAM wihthout setup, we'll use it to setup project and authorize
+bash <(curl -s -S -L https://git.io/install-gam) -l
 
 GAM=~/bin/gam/gam
 MEETREPORT=~/meet-report/
 OAUTHFILE=$MEETREPORT/oauth2.txt
 CLIENTSECRETS=$MEETREPORT/client_secrets.json
 
-$gam use project $GOOGLE_CLOUD_PROJECT
+$GAM use project $GOOGLE_CLOUD_PROJECT
 
 read -p "Enter your admin email address: " ADMINUSER
 
 SCOPES="https://www.googleapis.com/auth/gmail.send,https://www.googleapis.com/auth/admin.reports.audit.readonly,https://www.googleapis.com/auth/calendar.events.readonly
 ,email"
-$gam create oauth $ADMINUSER $SCOPES
+$GAM create oauth $ADMINUSER $SCOPES
 
 cp $MEETREPORT/config.py.example $MEETREPORT/config.py
 
